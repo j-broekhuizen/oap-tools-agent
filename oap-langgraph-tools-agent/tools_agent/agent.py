86590bfa5463be620fa59ef8a359ee082b7730d9
Jake Broekhuizen <jb@langchain.dev>
2025-05-16 17:01:46 -0700

    Add files via upload

---
+++ oap-langgraph-tools-agent/tools_agent/agent.py
@@ -0,0 +1,176 @@
+from langchain_core.runnables import RunnableConfig
+from typing import Optional, List
+from pydantic import BaseModel, Field
+from langgraph.prebuilt import create_react_agent
+from tools_agent.utils.tools import create_rag_tool
+from langchain.chat_models import init_chat_model
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from tools_agent.utils.token import fetch_tokens
+from tools_agent.utils.tools import wrap_mcp_authenticate_tool
+
+UNEDITABLE_SYSTEM_PROMPT = "\nIf the tool throws an error requiring authentication, provide the user with a Markdown link to the authentication page and prompt them to authenticate."
+
+DEFAULT_SYSTEM_PROMPT = (
+    "You are a helpful assistant that has access to a variety of tools."
+)
+
+
+class RagConfig(BaseModel):
+    rag_url: Optional[str] = None
+    """The URL of the rag server"""
+    collections: Optional[List[str]] = None
+    """The collections to use for rag"""
+
+
+class MCPConfig(BaseModel):
+    url: Optional[str] = Field(
+        default=None,
+        optional=True,
+    )
+    """The URL of the MCP server"""
+    tools: Optional[List[str]] = Field(
+        default=None,
+        optional=True,
+    )
+    """The tools to make available to the LLM"""
+
+
+class GraphConfigPydantic(BaseModel):
+    model_name: Optional[str] = Field(
+        default="anthropic:claude-3-7-sonnet-latest",
+        metadata={
+            "x_oap_ui_config": {
+                "type": "select",
+                "default": "anthropic:claude-3-7-sonnet-latest",
+                "description": "The model to use in all generations",
+                "options": [
+                    {
+                        "label": "Claude 3.7 Sonnet",
+                        "value": "anthropic:claude-3-7-sonnet-latest",
+                    },
+                    {
+                        "label": "Claude 3.5 Sonnet",
+                        "value": "anthropic:claude-3-5-sonnet-latest",
+                    },
+                    {"label": "GPT 4o", "value": "openai:gpt-4o"},
+                    {"label": "GPT 4o mini", "value": "openai:gpt-4o-mini"},
+                    {"label": "GPT 4.1", "value": "openai:gpt-4.1"},
+                ],
+            }
+        },
+    )
+    temperature: Optional[float] = Field(
+        default=0.7,
+        metadata={
+            "x_oap_ui_config": {
+                "type": "slider",
+                "default": 0.7,
+                "min": 0,
+                "max": 2,
+                "step": 0.1,
+                "description": "Controls randomness (0 = deterministic, 2 = creative)",
+            }
+        },
+    )
+    max_tokens: Optional[int] = Field(
+        default=4000,
+        metadata={
+            "x_oap_ui_config": {
+                "type": "number",
+                "default": 4000,
+                "min": 1,
+                "description": "The maximum number of tokens to generate",
+            }
+        },
+    )
+    system_prompt: Optional[str] = Field(
+        default=DEFAULT_SYSTEM_PROMPT,
+        metadata={
+            "x_oap_ui_config": {
+                "type": "textarea",
+                "placeholder": "Enter a system prompt...",
+                "description": f"The system prompt to use in all generations. The following prompt will always be included at the end of the system prompt:\n---{UNEDITABLE_SYSTEM_PROMPT}\n---",
+                "default": DEFAULT_SYSTEM_PROMPT,
+            }
+        },
+    )
+    mcp_config: Optional[MCPConfig] = Field(
+        default=None,
+        optional=True,
+        metadata={
+            "x_oap_ui_config": {
+                "type": "mcp",
+                # Here is where you would set the default tools.
+                # "default": {
+                #     "tools": ["Math_Divide", "Math_Mod"]
+                # }
+            }
+        },
+    )
+    rag: Optional[RagConfig] = Field(
+        default=None,
+        optional=True,
+        metadata={
+            "x_oap_ui_config": {
+                "type": "rag",
+                # Here is where you would set the default collection. Use collection IDs
+                # "default": {
+                #     "collections": [
+                #         "fd4fac19-886c-4ac8-8a59-fff37d2b847f",
+                #         "659abb76-fdeb-428a-ac8f-03b111183e25",
+                #     ]
+                # },
+            }
+        },
+    )
+
+
+async def graph(config: RunnableConfig):
+    cfg = GraphConfigPydantic(**config.get("configurable", {}))
+    tools = []
+
+    supabase_token = config.get("configurable", {}).get("x-supabase-access-token")
+    if cfg.rag and cfg.rag.rag_url and cfg.rag.collections and supabase_token:
+        for collection in cfg.rag.collections:
+            rag_tool = await create_rag_tool(
+                cfg.rag.rag_url, collection, supabase_token
+            )
+            tools.append(rag_tool)
+
+    if (
+        cfg.mcp_config
+        and cfg.mcp_config.url
+        and cfg.mcp_config.tools
+        and (mcp_tokens := await fetch_tokens(config))
+    ):
+        mcp_client = MultiServerMCPClient(
+            connections={
+                "mcp_server": {
+                    "transport": "streamable_http",
+                    "url": cfg.mcp_config.url.rstrip("/") + "/mcp",
+                    "headers": {
+                        "Authorization": f"Bearer {mcp_tokens['access_token']}"
+                    },
+                }
+            }
+        )
+        tools.extend(
+            [
+                wrap_mcp_authenticate_tool(tool)
+                for tool in await mcp_client.get_tools()
+                if tool.name in cfg.mcp_config.tools
+            ]
+        )
+
+    model = init_chat_model(
+        cfg.model_name,
+        temperature=cfg.temperature,
+        max_tokens=cfg.max_tokens,
+    )
+
+    return create_react_agent(
+        prompt=cfg.system_prompt + UNEDITABLE_SYSTEM_PROMPT,
+        model=model,
+        tools=tools,
+        config_schema=GraphConfigPydantic,
+    )
